28e3dd927c3d7ff0c0f2f7a1b7c00ef15e222ba9
Chandan Kumar Shaw <66195931+i-am-Chandan-Shaw@users.noreply.github.com>
2020-06-10 00:42:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ function press(key){
 				s.play();
 				break;
 			case "j":
-				var s= new Audio("tom-2.mp3");
+				var s= new Audio("tom-3.mp3");
 				s.play();
 				break;
 			case "k":
-				var s= new Audio("tom-3.mp3");
+				var s= new Audio("tom-2.mp3");
 				s.play();
 				break;
 			case "l":
