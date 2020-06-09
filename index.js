f6f47dc4c788494d3f85fe5543e7448e21d6f0ea
Chandan Kumar Shaw <66195931+i-am-Chandan-Shaw@users.noreply.github.com>
2020-06-10 00:23:28 +0530

    A working drumkit

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+var l=document.querySelectorAll(".drum").length;
+
+for (i=0; i<l;i++){
+	document.querySelectorAll("button")[i].addEventListener("click",handleClick);
+
+}
+
+
+function handleClick(){
+	var i=this.innerHTML;
+	press(i);
+	anim(i);
+	}
+
+
+document.addEventListener("keypress",function(event){
+	press(event.key);
+	anim(event.key);
+})
+function press(key){  
+		switch (key){
+			case "w":
+				var s= new Audio("sounds/crash.mp3");
+				s.play();
+				break;
+			case "a":
+				var s= new Audio("sounds/kick-bass.mp3");
+				s.play();
+				break;
+			case "s":
+				var s= new Audio("sounds/snare.mp3");
+				s.play();
+				break;
+			case "d":
+				var s= new Audio("sounds/tom-1.mp3");
+				s.play();
+				break;
+			case "j":
+				var s= new Audio("sounds/tom-2.mp3");
+				s.play();
+				break;
+			case "k":
+				var s= new Audio("sounds/tom-3.mp3");
+				s.play();
+				break;
+			case "l":
+				var s= new Audio("sounds/tom-4.mp3");
+				s.play();
+				break;
+			default: console.log(i);
+
+
+		}
+	}
+
+function anim(cur){
+	var active=document.querySelector("." + cur);
+	active.classList.add("pressed");
+	setTimeout(function() {
+		active.classList.remove("pressed");
+	},100);
+
+}
